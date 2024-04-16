6f6993fdf09f50162aeb6fa9519fe53ed24258af
daytonJA <dayton.jacob.n@gmail.com>
2024-04-16 10:11:19 -0400

    Initial commit

---
+++ R/find_movements.R
@@ -0,0 +1,321 @@
+#' Find movements
+#'
+#' This function identifies all "blobs" or movements between consecutive images
+#' @param files a vector of file names
+#' @export
+#' @examples
+#' # out <- find_movements(files = file_names,n_max = 75000,find_thr = T,type_thr = "absolute",p_sample = 0.2,channel = "grayscale", animal = "white")
+#'
+find_movements <- function(files,
+                           n_thr = 0.996,
+                           n_cln = 5,
+                           n_grw = 1.5,
+                           n_blr = 3,
+                           n_max = 75000,
+                           x_left = 0,
+                           x_right = 2592,
+                           y_bot = 0,
+                           y_top = 1944,
+                           find_thr = T,
+                           type_thr = "absolute",
+                           p_sample = 0.02,
+                           channel = "grayscale",
+                           animal = "white") {
+
+  if (find_thr == T & type_thr == "relative") {
+    stop("The find_threshold argument only obtains absolute, not relative, threshold values.
+               Please select find_thr='T' AND type_thr='absolute'.
+               or choose find_thr='F'AND type_thr='relative'")
+  }
+
+  if (find_thr == T & type_thr == "absolute") {
+    print("find_thr==T, type_thr==absolute")
+    pix_diff <- find_threshold(files=files[1:(0.5*length(files))], p_sample=p_sample,
+                               n_blr=n_blr, n_thr=n_thr,
+                               x_left=x_left, x_right=x_right,
+                               y_bot=y_bot, y_top=y_top,
+                               channel=channel, animal=animal)
+
+    n_sample <- length(pix_diff) # sample size
+    n_bootstrap <- 1000 # number of bootstrap samples
+    bootstrap <- c() # empty vector to hold results
+
+    for (i in 1:n_bootstrap){
+      obs <- sample(1:n_sample, replace = T)
+      bootstrap[i] <- mean(pix_diff[obs]) # mean of the bootstrap sample
+    }
+
+    pix_boot = quantile(bootstrap, 0.99) # 99% value for bootstrap mean ***
+    n_thr = pix_boot
+  }
+
+  n <- length(files) # number of images we have to look at is called n
+
+  mvmt <- tibble::tibble(s = NA,
+                         x = NA,
+                         y = NA,
+                         d = NA)
+
+  blnk <- tibble::tibble(s = 0,
+                         x = NA,
+                         y = NA)
+
+  time_prev <- files[1]
+
+  if (channel == "R" | channel == "red") {
+    prev <- imager::load.image(time_prev) %>% imager::R() %>% imager::isoblur(sigma = n_blr)
+    i <- 1
+    while (i <= (n-1)) {
+      time_post <- files[i+1]
+      post <- imager::load.image(time_post) %>% imager::R() %>% imager::isoblur(sigma = n_blr)
+
+      if (animal == "black") { # if animal is black on white background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (prev - post) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      } else { # if animal is white on black background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (post - prev) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      }
+
+      if (find_thr == F & type_thr == "relative") {
+        pixs <- diff %>% imager::threshold(stringr::str_c(n_thr*100,"%"))
+      } else {
+        pixs <- (diff > n_thr)
+      }
+
+      { setTimeLimit(45) # stops automatically after 30s (it shouldn't take that long)
+        pixs <- pixs %>%
+          imager::shrink(n_cln) %>%
+          imager::grow(n_grw*n_cln) }
+
+      if (sum(pixs) > 0 & sum(pixs) < n_max) { # white pixels of interest must be >0 and <75000 (too noisy)
+        splt <- pixs %>% imager::split_connected() # returns discrete blobs of connected pixels
+        if (length(splt) > 0) { # number of discrete movements ("blobs") must be > 0
+          blbs <- tibble::tibble(s = rep(NA, length(splt)),
+                                 x = rep(NA, length(splt)),
+                                 y = rep(NA, length(splt)),
+                                 d = rep(NA, length(splt)))
+          for (j in 1:length(splt)) {
+            blbs$s[j] <- sum(splt[[j]])
+            blbs$x[j] <- imager::where(splt[[j]]) %>% # table of all pixel x- and y-coordinates
+              dplyr::summarize(mean(x)) %>%          # takes mean value of column x (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$y[j] <- imager::where(splt[[j]]) %>%
+              dplyr::summarize(mean(y)) %>%          # takes mean value of column y (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$d[j] <- time_post
+          }
+          mvmt <- rbind(mvmt, blbs)
+        } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+      } else if (sum(pixs) >= n_max) { # image is noisy, we assign 2000000 to remove later
+        blbs <- tibble::tibble(s = 2000000,
+                               x = 0,
+                               y = 0,
+                               d = time_post)
+        mvmt <- rbind(mvmt, blbs)
+      } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+
+      time_prev <- time_post # new time becomes old time
+      prev <- post # new photo becomes old photo
+      i <- i + 1   # increment i by 1 (part of while loop)
+      suppressWarnings(rm(diff, pixs, splt, blbs, post))
+    }}
+  else if (channel == "G" | channel == "green") {
+    prev <- imager::load.image(time_prev) %>% imager::G() %>% imager::isoblur(sigma = n_blr)
+    i <- 1
+    while (i <= (n-1)) {
+      time_post <- files[i+1]
+      post <- imager::load.image(time_post) %>% imager::G() %>% imager::isoblur(sigma = n_blr)
+
+      if (animal == "black") { # if animal is black on white background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (prev - post) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      } else { # if animal is white on black background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (post - prev) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      }
+
+      if (find_thr == F & type_thr == "relative") {
+        pixs <- diff %>% imager::threshold(stringr::str_c(n_thr*100,"%"))
+      } else {
+        pixs <- (diff > n_thr)
+      }
+
+      { setTimeLimit(20) # stops automatically after 30s (it shouldn't take that long)
+        pixs <- pixs %>%
+          imager::shrink(n_cln) %>%
+          imager::grow(n_grw*n_cln) }
+
+      if (sum(pixs) > 0 & sum(pixs) < n_max) { # white pixels of interest must be >0 and <75000 (too noisy)
+        splt <- pixs %>% imager::split_connected() # returns discrete blobs of connected pixels
+        if (length(splt) > 0) { # number of discrete movements ("blobs") must be > 0
+          blbs <- tibble::tibble(s = rep(NA, length(splt)),
+                                 x = rep(NA, length(splt)),
+                                 y = rep(NA, length(splt)),
+                                 d = rep(NA, length(splt)))
+          for (j in 1:length(splt)) {
+            blbs$s[j] <- sum(splt[[j]])
+            blbs$x[j] <- imager::where(splt[[j]]) %>% # table of all pixel x- and y-coordinates
+              dplyr::summarize(mean(x)) %>%          # takes mean value of column x (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$y[j] <- imager::where(splt[[j]]) %>%
+              dplyr::summarize(mean(y)) %>%          # takes mean value of column y (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$d[j] <- time_post
+          }
+          mvmt <- rbind(mvmt, blbs)
+        } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+      } else if (sum(pixs) >= n_max) { # image is noisy, we assign 2000000 to remove later
+        blbs <- tibble::tibble(s = 2000000,
+                               x = 0,
+                               y = 0,
+                               d = time_post)
+        mvmt <- rbind(mvmt, blbs)
+      } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+
+      time_prev <- time_post # new time becomes old time
+      prev <- post # new photo becomes old photo
+      i <- i + 1   # increment i by 1 (part of while loop)
+      suppressWarnings(rm(diff, pixs, splt, blbs, post))
+    }
+  }
+  else if (channel == "B" | channel == "blue") {
+    prev <- imager::load.image(time_prev) %>% imager::B() %>% imager::isoblur(sigma = n_blr)
+    i <- 1
+    while (i <= (n-1)) {
+      time_post <- files[i+1]
+      post <- imager::load.image(time_post) %>% imager::B() %>% imager::isoblur(sigma = n_blr)
+
+      if (animal == "black") { # if animal is black on white background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (prev - post) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      } else { # if animal is white on black background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (post - prev) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      }
+
+      if (find_thr == F & type_thr == "relative") {
+        pixs <- diff %>% imager::threshold(stringr::str_c(n_thr*100,"%"))
+      } else {
+        pixs <- (diff > n_thr)
+      }
+
+      { setTimeLimit(20) # stops automatically after 30s (it shouldn't take that long)
+        pixs <- pixs %>%
+          imager::shrink(n_cln) %>%
+          imager::grow(n_grw*n_cln) }
+
+      if (sum(pixs) > 0 & sum(pixs) < n_max) { # white pixels of interest must be >0 and <75000 (too noisy)
+        splt <- pixs %>% imager::split_connected() # returns discrete blobs of connected pixels
+        if (length(splt) > 0) { # number of discrete movements ("blobs") must be > 0
+          blbs <- tibble::tibble(s = rep(NA, length(splt)),
+                                 x = rep(NA, length(splt)),
+                                 y = rep(NA, length(splt)),
+                                 d = rep(NA, length(splt)))
+          for (j in 1:length(splt)) {
+            blbs$s[j] <- sum(splt[[j]])
+            blbs$x[j] <- imager::where(splt[[j]]) %>% # table of all pixel x- and y-coordinates
+              dplyr::summarize(mean(x)) %>%          # takes mean value of column x (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$y[j] <- imager::where(splt[[j]]) %>%
+              dplyr::summarize(mean(y)) %>%          # takes mean value of column y (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$d[j] <- time_post
+          }
+          mvmt <- rbind(mvmt, blbs)
+        } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+      } else if (sum(pixs) >= n_max) { # image is noisy, we assign 2000000 to remove later
+        blbs <- tibble::tibble(s = 2000000,
+                               x = 0,
+                               y = 0,
+                               d = time_post)
+        mvmt <- rbind(mvmt, blbs)
+      } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+
+      time_prev <- time_post # new time becomes old time
+      prev <- post # new photo becomes old photo
+      i <- i + 1   # increment i by 1 (part of while loop)
+      suppressWarnings(rm(diff, pixs, splt, blbs, post))
+    }
+  }
+  else {
+    prev <- imager::load.image(time_prev) %>% imager::grayscale() %>% imager::isoblur(sigma = n_blr)
+    i <- 1
+    while (i <= (n-1)) {
+      time_post <- files[i+1]
+      post <- imager::load.image(time_post) %>% imager::grayscale() %>% imager::isoblur(sigma = n_blr)
+
+      if (animal == "black") { # if animal is black on white background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (prev - post) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      } else { # if animal is white on black background, white pixels are PRESENT in post and ABSENT in prev
+        diff <- (post - prev) %>%
+          imager::imsub(x %inr% c(x_left,x_right),
+                        y %inr% c(y_bot,y_top)) # crop
+      }
+
+      if (find_thr == F & type_thr == "relative") {
+        pixs <- diff %>% imager::threshold(stringr::str_c(n_thr*100,"%"))
+      } else {
+        pixs <- (diff > n_thr)
+      }
+
+      { setTimeLimit(20) # stops automatically after 30s (it shouldn't take that long)
+        pixs <- pixs %>%
+          imager::shrink(n_cln) %>%
+          imager::grow(n_grw*n_cln) }
+
+      if (sum(pixs) > 0 & sum(pixs) < n_max) { # white pixels of interest must be >0 and <75000 (too noisy)
+        splt <- pixs %>% imager::split_connected() # returns discrete blobs of connected pixels
+        if (length(splt) > 0) { # number of discrete movements ("blobs") must be > 0
+          blbs <- tibble::tibble(s = rep(NA, length(splt)),
+                                 x = rep(NA, length(splt)),
+                                 y = rep(NA, length(splt)),
+                                 d = rep(NA, length(splt)))
+          for (j in 1:length(splt)) {
+            blbs$s[j] <- sum(splt[[j]])
+            blbs$x[j] <- imager::where(splt[[j]]) %>% # table of all pixel x- and y-coordinates
+              dplyr::summarize(mean(x)) %>%          # takes mean value of column x (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$y[j] <- imager::where(splt[[j]]) %>%
+              dplyr::summarize(mean(y)) %>%          # takes mean value of column y (coordinate)
+              as.numeric() %>%              # extracts numerical value from mini-table
+              round(0) #***
+            blbs$d[j] <- time_post
+          }
+          mvmt <- rbind(mvmt, blbs)
+        } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+      } else if (sum(pixs) >= n_max) { # image is noisy, we assign 2000000 to remove later
+        blbs <- tibble::tibble(s = 2000000,
+                               x = 0,
+                               y = 0,
+                               d = time_post)
+        mvmt <- rbind(mvmt, blbs)
+      } else { mvmt <- rbind(mvmt, cbind(blnk, d = time_post)) }
+
+      time_prev <- time_post # new time becomes old time
+      prev <- post # new photo becomes old photo
+      i <- i + 1   # increment i by 1 (part of while loop)
+      suppressWarnings(rm(diff, pixs, splt, blbs, post))
+    }}
+
+  suppressWarnings(rm(blnk, blbs, diff, time_prev, time_post))
+  mvmt$d <- sub("\\.[[:alnum:]]+$", "", basename(as.character(mvmt$d)))
+  mvmt = mvmt[-1,]
+
+  return(mvmt)
+}
---
+++ R/find_threshold.R
@@ -0,0 +1,71 @@
+#' Find pixel difference cutoff for thresholding
+#'
+#' This function is called within find_movements()
+#' @export
+#' @examples
+#'  pix_diff <- find_threshold(files=files[1:(0.5*length(files))], p_sample=p_sample,n_blr=n_blr, n_thr=n_thr,x_left=x_left, x_right=x_right,y_bot=y_bot, y_top=y_top,channel=channel, animal=animal)
+#'
+find_threshold <- function(files,                                       # list of file names
+                           p_sample = 0.05,                # percent of images to sample ***
+                           n_thr = 0.999,                      # n_threshold integer (e.g., 0.996 == "0.4%" )
+                           n_blr = 3,                          # let user select blur radius? ***
+                           x_left = 0,                                  # value for crop on x min
+                           x_right = 2592,                              # value for crop on x max
+                           y_bot = 0,                                   # value for crop on y min
+                           y_top = 1944,                                # value for crop on y max
+                           channel = "grayscale", # channel value
+                           animal = "white"){               # white on black bg or black on white bg
+
+  n <- length(files) # total number of images to be analyzed
+
+  iterations <- ceiling(p_sample*n)
+  pix_diff <- NA # empty vector to hold results
+
+  for (i in 1:iterations){
+    pix_sample <- sample((n-1), 1, replace = T) # randomly sample "prev" image *
+
+    if (channel == "R" | channel == "red") {
+      prev <-
+        imager::load.image(files[pix_sample]) %>%
+        imager::R() %>% imager::isoblur(sigma = n_blr)
+      post <-
+        imager::load.image(files[pix_sample+1]) %>%
+        imager::R() %>% imager::isoblur(sigma = n_blr)
+    } else if (channel == "G" | channel == "green") {
+      prev <-
+        imager::load.image(files[pix_sample]) %>%
+        imager::G() %>% imager::isoblur(sigma = n_blr)
+      post <-
+        imager::load.image(files[pix_sample+1]) %>%
+        imager::G() %>% imager::isoblur(sigma = n_blr)
+    } else if (channel == "B" | channel == "blue") {
+      prev <-
+        imager::load.image(files[pix_sample]) %>%
+        imager::B() %>% imager::isoblur(sigma = n_blr)
+      post <-
+        imager::load.image(files[pix_sample+1]) %>%
+        imager::B() %>% imager::isoblur(sigma = n_blr)
+    } else {
+      prev <-
+        imager::load.image(files[pix_sample]) %>%
+        imager::grayscale() %>% imager::isoblur(sigma = n_blr)
+      post <-
+        imager::load.image(files[pix_sample+1]) %>%
+        imager::grayscale() %>% imager::isoblur(sigma = n_blr)
+    }
+
+    if (animal == "black") {
+      diff <- (prev - post) %>%
+        imager::imsub(x %inr% c(x_left, x_right),
+                      y %inr% c(y_bot, y_top)) # crop
+      pix_diff[i] <- quantile(diff, n_thr) # pixel difference value for corresponding n_thr quantile
+    } else if (animal == "white") { # if animal is white on black background, white pixels are PRESENT in post and ABSENT in prev
+      diff <- (post - prev) %>%
+        imager::imsub(x %inr% c(x_left, x_right),
+                      y %inr% c(y_bot, y_top))}
+    pix_diff[i] <- quantile(diff, n_thr) # pixel difference value for corresponding n_thr quantile
+  }
+  print("Completed find_threshold")
+  print(mean(pix_diff))
+  return(pix_diff)
+}
