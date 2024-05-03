a61cf0cfe13b360424c2d01021479306a87b4c28
iLAMtools <imaging.lam.mothra@gmail.com>
2024-05-03 12:43:03 -0400

    moved circle standards down

--- R/plot_movements.R
+++ R/plot_movements.R
@@ -42,27 +42,27 @@ plot_movements <- function(file_names,
 
     #Draw circle standards for blob circle ~12800, 3200, 800, 200, 50 pxs
     draw.circle(75, #x coord
-                75, #y coord
+                (y_top-y_bot)-75, #y coord
                 sqrt(12800/base::pi),
                 border = 'green',
                 lwd=3)
     draw.circle(175, #x coord
-                43, #y coord
+                (y_top-y_bot)-43, #y coord
                 sqrt(3200/base::pi),
                 border = 'green',
                 lwd=3)
     draw.circle(227, #x coord
-                27, #y coord
+                (y_top-y_bot)-27, #y coord
                 sqrt(800/base::pi),
                 border = 'green',
                 lwd=3)
     draw.circle(255, #x coord
-                19, #y coord
+                (y_top-y_bot)-19, #y coord
                 sqrt(200/base::pi),
                 border = 'green',
                 lwd=3)
     draw.circle(271, #x coord
-                15, #y coord
+                (y_top-y_bot)-15, #y coord
                 sqrt(50/base::pi),
                 border = 'green',
                 lwd=3)
